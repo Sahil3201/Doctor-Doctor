70b04e25a5b106f085ed05bbe5a40e3892a5c1a6
Sahil Lunawat <45628585+Sahil3201@users.noreply.github.com>
2020-11-03 01:53:07 +0530

    .

--- DoctorDoctor/views.py
+++ DoctorDoctor/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 def home(request):
-	return render(request,'DoctorDoctor/home.html')
+	if request.method=='POST':
+		return render(request,'DoctorDoctor/home.html')
+	else:
+		return render(request,'DoctorDoctor/home.html')
 
 class departments(TemplateView):
 	template_name='DoctorDoctor/departments.html'
