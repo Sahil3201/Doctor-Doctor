eebff3dc6bf379e4f20222754cfefc03b36b053d
Sahil Lunawat <45628585+Sahil3201@users.noreply.github.com>
2020-11-20 11:57:22 +0530

    Configuration for heroku

--- DoctorDoctor/settings.py
+++ DoctorDoctor/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-#import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,4 +147,4 @@ EMAIL_HOST_USER = 'doctordoctor3201@gmail.com'
 EMAIL_HOST_PASSWORD = 'doctor3201'
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
